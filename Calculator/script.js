f9295f91e7fc1cc9d73cd5d494fb2013856b58d6
SunnyPatel2011 <mikeshake99099@gamil.com>
2024-03-13 15:48:17 +0530

    new changes

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,74 +1,75 @@
-let Display = document.getElementById('Display');
+let display = document.getElementById('Display');
 let historyContainer = document.getElementById('history');
 let trashicon = document.getElementById('trashicon');
 let Bracket = true;
 function Result(value) {
-    Display.value += value;
+    display.value += value;
 }
 
 
 function clearResult() {
-    Display.value = '';
+    display.value = '';
 }
 
 function clearEntry() {
-   Display.value = Display.value.substring(0, Display.value.length-1);   
+    display.value = display.value.substring(0, display.value.length - 1);
 }
 
 
 function calculate() {
     try {
-        let expression = Display.value;
+        let expression = display.value;
         if (expression.includes('%')) {
             const parts = expression.split('%');
             const percentage = parseFloat(parts[0]);
             const number = parseFloat(parts[1]);
-           let answer = (percentage / 100) * number;
-            Display.value = answer;
+            let answer = (percentage / 100) * number;
+            display.value = answer;
             let his = expression + ' = ' + answer;
             saveHistory(his);
         } else {
+
             let answer = eval(expression);
-            Display.value = answer;
-            let his = expression + ' = '+ answer;
+            display.value = answer;
+            let his = expression + ' = ' + answer;
             saveHistory(his);
         }
     } catch (error) {
-        Display.value = 'Error';
+        display.value = 'Error';
     }
 }
- 
+
 function saveHistory(his) {
-    let history = document.createElement('p');    
-    let parts = his.match(/.{1,33}/g); 
+    let history = document.createElement('p');
+    let parts = his.match(/.{1,33}/g);
     history.textContent = parts.join('\n');
     historyContainer.appendChild(history);
 }
 
 
 function focusScreen() {
-    Display.focus();
+    display.focus();
 }
 
 
 function Result(val) {
     if (val === '()') {
         if (Bracket) {
-            Display.value += '(';
+            display.value += '(';
             Bracket = true;
         } else {
-            Display.value += ')';
+            display.value += ')';
             Bracket = false;
         }
         Bracket = !Bracket;
     } else {
-        Display.value += val;
+        display.value += val;
     }
 }
 
 
 trashicon.addEventListener('click', deletehistory);
-function deletehistory(){
+function deletehistory() {
     historyContainer.innerHTML = '';
     location.reload();
 }
@@ -79,16 +80,16 @@ document.addEventListener('keydown', (event) => {
         Result(key);
         focusScreen();
 
-    }else if (key === 'Enter') {
-            calculate();
-        
+    } else if (key === 'Enter') {
+        calculate();
+
     } else if (key === 'Backspace') {
         clearEntry();
         focusScreen();
     } else if (key === 'Escape') {
         clearResult();
         focusScreen();
-    }else if (key === 'Delete') {
+    } else if (key === 'Delete') {
         deletehistory();
         focusScreen();
     }
